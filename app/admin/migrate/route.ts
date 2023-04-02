6822dc8fc659f02f6b6fd8bde37156dca04c143d
Jon Smithers <jon@smithers.dev>
2023-04-02 14:30:31 -0400

    Force dynamic routes
    Add vercel

--- app/admin/migrate/route.ts
+++ app/admin/migrate/route.ts
@@ -1,6 +1,8 @@
 import { getDb } from '@/app/db/connection';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(_request: Request) {
   try {
     await migrate(getDb(), { migrationsFolder: './migrations' });
--- app/admin/test-db/route.ts
+++ app/admin/test-db/route.ts
@@ -1,6 +1,8 @@
 import { getDb } from "@/app/db/connection";
 import { users } from "@/app/db/schema";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(_request: Request) {
   const db = getDb();
   const allUsers = await db.select().from(users);
