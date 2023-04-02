360038d3420b45ca7d81ed9d56aa4e851045d7d5
Jon Smithers <jon@smithers.dev>
2023-04-02 15:07:05 -0400

    Run migrations on build?

--- app/db/run-migrations/route.ts
+++ app/db/run-migrations/route.ts
@@ -1,12 +1,12 @@
 import { getDb } from '@/app/db/connection';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 
-export async function GET(_request: Request) {
+export async function GET(request: Request) {
   try {
     await migrate(getDb(), { migrationsFolder: './migrations' });
-    return new Response('migrated');
+    return new Response('migration successful');
   } catch (e) {
     console.error(e);
     return new Response(String(e));
