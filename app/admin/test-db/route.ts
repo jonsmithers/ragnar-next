257b3f1bc08742006ae1727b4130114bb68e7aa2
Jon Smithers <jon@smithers.dev>
2023-04-04 09:03:15 -0400

    Make things not null

--- app/admin/test-db/route.ts
+++ app/admin/test-db/route.ts
@@ -1,11 +1,10 @@
 import { getDb } from '@/app/db/connection';
-import { teams, users } from '@/app/db/schema';
+import { teams } from '@/app/db/schema';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(_request: Request) {
   const db = getDb();
-  const allUsers = await db.select().from(users);
   const allTeams = await db.select().from(teams);
-  return new Response(JSON.stringify({ allUsers, allTeams }));
+  return new Response(JSON.stringify({ allTeams }));
 }
--- app/db/schema.ts
+++ app/db/schema.ts
@@ -8,29 +8,29 @@ import {
 } from 'drizzle-orm/mysql-core';
 
 export const teams = mysqlTable('teams', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 256 }),
-  startTime: time('start_time'),
-  trailRunMultiplierLow: decimal('trail_run_multiplier_low'),
-  trailRunMultiplierHigh: decimal('trail_run_multiplier_high'),
+  id: serial('id').primaryKey().notNull(),
+  name: varchar('name', { length: 256 }).notNull(),
+  startTime: time('start_time').notNull(),
+  trailRunMultiplierLow: decimal('trail_run_multiplier_low').notNull(),
+  trailRunMultiplierHigh: decimal('trail_run_multiplier_high').notNull(),
 });
 
 export const runners = mysqlTable('runners', {
-  id: serial('id').primaryKey(),
-  order: int('order'),
-  name: varchar('name', { length: 256 }),
-  pace10k: time('pace_10k'),
+  id: serial('id').primaryKey().notNull(),
+  order: int('order').notNull(),
+  name: varchar('name', { length: 256 }).notNull(),
+  pace10k: time('pace_10k').notNull(),
 });
 
 export const loops = mysqlTable('loops', {
-  id: serial('id').primaryKey(),
-  order: int('order'),
-  name: varchar('name', { length: 256 }),
-  lengthMiles: decimal('length_miles'),
+  id: serial('id').primaryKey().notNull(),
+  order: int('order').notNull(),
+  name: varchar('name', { length: 256 }).notNull(),
+  lengthMiles: decimal('length_miles').notNull(),
 });
 
 export const actualFinishTimes = mysqlTable('actual_finish_times', {
-  id: serial('id').primaryKey(),
-  runnerId: serial('id'),
-  loopId: serial('id'),
+  id: serial('id').primaryKey().notNull(),
+  runnerId: serial('id').notNull(),
+  loopId: serial('id').notNull(),
 });
