07c04f2ebede9e0be1b44b6989387010c38e54a0
Jon Smithers <jon@smithers.dev>
2023-04-12 21:24:01 -0400

    Force dynamic migrations

--- app/db/run-migrations/route.ts
+++ app/db/run-migrations/route.ts
@@ -1,7 +1,7 @@
 import { getDb } from '@/app/db/connection';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
   try {
